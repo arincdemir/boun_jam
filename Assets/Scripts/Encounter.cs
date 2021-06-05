5ff055ef158e88dd9afbcf8bb273b2cb0b07f668
arincdemir <68223368+arincdemir@users.noreply.github.com>
2021-06-05 15:09:36 +0300

    Encounters diye scriptableobj oluşturdum

---
+++ Assets/Scripts/Encounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewEncounter", menuName = "ScriptableObjects/Encounter")]
+public class Encounter : ScriptableObject
+{
+    public Sprite image;
+    public string advisorName;
+    public string text;
+    public int para;
+    public int çıkar;
+    public int korku;
+    public int asker;
+}
