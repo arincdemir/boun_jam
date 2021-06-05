eb0944c10222b4e2726738dfdfb5baf236278805
arincdemir <68223368+arincdemir@users.noreply.github.com>
2021-06-05 15:49:40 +0300

    Encounterler çalışıyor
    Butonları gerekli yerlere bağladım artık gamemanager'a yüklenen encounterlar sıra sıra gelecek ve sonuçları playerdatas scriptini etkileyecek

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Image advisorImage;
+    public Text encounterText;
+    public PlayerDatas playerDatas;
+
+    public Encounter[] encounters;
+    Encounter currentEncounter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadNewEncounter();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    void LoadNewEncounter()
+    {
+        // I subtracted 0.5 in order to make all posibilities the same.
+        int encounterIndex = Mathf.RoundToInt(Random.Range(0, encounters.Length) - 0.5f);
+        currentEncounter = encounters[encounterIndex];
+        advisorImage.sprite = currentEncounter.image;
+        encounterText.text = currentEncounter.text;
+    }
+
+    public void ProposalAccepted()
+    {
+        playerDatas.para += currentEncounter.para;
+        playerDatas.çıkar += currentEncounter.çıkar;
+        playerDatas.korku += currentEncounter.korku;
+        playerDatas.asker += currentEncounter.asker;
+
+        Save();
+        LoadNewEncounter();
+    }
+
+    public void ProposalDeclined()
+    {
+        playerDatas.para -= currentEncounter.para;
+        playerDatas.çıkar -= currentEncounter.çıkar;
+        playerDatas.korku -= currentEncounter.korku;
+        playerDatas.asker -= currentEncounter.asker;
+
+        Save();
+        LoadNewEncounter();
+    }
+
+    void Save()
+    {
+
+    }
+
+    private void Load()
     {
         
     }
---
+++ Assets/Scripts/PlayerDatas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDatas : MonoBehaviour
+{
+    public int day = 0;
+    public int para;
+    public int çıkar;
+    public int korku;
+    public int asker;
+}
