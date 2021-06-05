6420a25ed73c92f7b41156ffb3a61bbbdbc08ccb
arincdemir <68223368+arincdemir@users.noreply.github.com>
2021-06-05 22:23:11 +0300

    HighScore mainMenu de gözüküyor
    Highscore ve lastscore mainmenude gözüküyor

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public int settingsBuildIndex;
-    public int gameBuildIndex;
+    public Text highScoreText;
+    public Text lastScoreText;
     // Start is called before the first frame update
     void Start()
     {
         PlayerPrefs.SetInt("HighScore", Mathf.Max(PlayerPrefs.GetInt("HighScore"), PlayerPrefs.GetInt("LastScore")));
+        highScoreText.text = highScoreText.text + " " +PlayerPrefs.GetInt("HighScore");
+        lastScoreText.text = lastScoreText.text + " " + PlayerPrefs.GetInt("LastScore");
     }
 
     // Update is called once per frame
